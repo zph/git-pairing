eba7d94aeec17b8d357b2f943835157c1c990224
asegal / squince <asegal@glgroup.com , squince@glgroup.com>
2013-05-09 17:14:14 -0400

    getting closer - need unicode to work

--- shell/.git-pairing-prompt.sh
+++ shell/.git-pairing-prompt.sh
@@ -1,24 +1,21 @@
 # bash git pairing prompt support
 #     - based on git-prompt.sh @ https://github.com/git/git/tree/master/contrib/completion
 
-#export PS1='  \e[0;32m  [\t \u  \e[0;34m   \W  \e[0;32m  ]  \e[0;32m  \$  \e[m  '
-#export PS1='\[\e[0;32m\][\t \u\[\e[0;34m\] \W\[\e[0;32m\]]\[\e[0;32m\]\$\[\e[m\]'
-
-
 __git_pairing_prompt ()
 {
   local c_clear="\[\e[m\]"
   local c_red="\[\e[0;32m\]"
   local c_green="\[\e[0;34m\]"
   local untracked="\[\u2733\]"
-  local pull_arrow="\[\u2798\]"
-  local push_arrow="\[\u279A\]"
+  local pull_arrow="\x25BE" #"\xE2\x96\xB4"
+  local push_arrow="\x25B4" #\xE2\x96\xBE"
 
   local d="$(pwd  2>/dev/null)"; # d = current working directory
-  local b="$(git symbolic-ref HEAD 2>/dev/null)"; # b = branch
+  local tb="$(git symbolic-ref HEAD 2>/dev/null)";
+  local b="${tb##refs/heads/}"; # b = branch
   local p="$(git config --get user.initials 2>/dev/null)"; # p = pair initials
   local s="$(git status --ignore-submodules --porcelain 2>/dev/null)"; # s = git status
-  local r="$(git remote >/dev/null 2>/dev/null)/$b"; # r = remote
+  local r="$(git remote 2>/dev/null)/$b"; # r = remote
   local push_pull="$(git rev-list --left-right $r...HEAD 2>/dev/null)"; # push_pull = list of revisions
   local push_count=0
   local pull_count=0
@@ -26,7 +23,7 @@ __git_pairing_prompt ()
   if [ -n "$push_pull" ]
   then
 	  local commit
-	  for commit in $push_pull
+	  for commit in "$push_pull"
 	  do
 		  case "$commit" in
 		  "<"*) ((pull_count++)) ;;
@@ -35,6 +32,8 @@ __git_pairing_prompt ()
 	  done
   fi
 
+  echo push: $push_count
+  echo pull: $pull_count
 
   # COMPONENTS OF PROMPT
   local prompt=""       # the prompt's final form
@@ -45,17 +44,17 @@ __git_pairing_prompt ()
   local u_prompt=""     # untracked files
   local d_prompt="$d"     # directory portion
 
-  if [ -n "$b" ]; then
-    b_prompt=" [${b##refs/heads/}]"
-  fi
   if [ -n "$p" ]; then
     p_prompt="${p}"
   fi
   if [ "$push_count" -gt 0  ]; then
-    ahead_r=""
+    ahead_r=" ${push_arrow}${push_count}"
   fi
   if [ "$pull_count" -gt 0  ]; then
-    behind_r=""
+    behind_r=" ${pull_arrow}${pull_count}"
+  fi
+  if [ -n "$b" ]; then
+    b_prompt=" [${b}${ahead_r}${behind_r}]"
   fi
 
   prompt+="${d_prompt}"
